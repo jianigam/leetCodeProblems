86a3a0a4a86c45e93538626a08cb85c5901f64cb
Jia Nigam <jia.nigam02@gmail.com>
2026-04-03 20:19:21 +0530

    Implement twoSum function using unordered_map

---
+++ twoSum/chatgptTwoSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int, int> mp; // value -> index
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
